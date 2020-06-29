d9b68e15b366d4de1f93dbb5e065a62527dd812b
Jonah Ullman <jonahmullman@gmail.com>
2020-06-28 19:59:56 -0500

    added packages, webpack, babel config, basic express server

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Log requests
+app.use(morgan('dev'));
+
+// Serve up static files
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Parse incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) =>
+  path.extname(req.path).length > 0 ? res.status(404).send('Not found') : next()
+);
+
+app.use('*', (req, res, next) =>
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+);
+
+app.use((err, req, res, next) =>
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+);
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+module.exports = app;
